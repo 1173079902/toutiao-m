eb65a4083d7598063a4dda4ffcab995a78cefa34
1173079902 <1173079902@qq.com>
2021-02-23 14:58:35 +0800

    使用Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
-    <router-view />
     <i class="toutiao toutiaoshanchu"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <router-view />
   </div>
 </template>
 

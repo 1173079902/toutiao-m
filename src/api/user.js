9f87ff588650c2fa25e6eb7a29a15e8b28625066
1173079902 <1173079902@qq.com>
2021-02-24 15:42:22 +0800

    获取验证码

--- src/api/user.js
+++ src/api/user.js
@@ -7,3 +7,11 @@ export const login = data => {
     data
   })
 }
+
+// 发送验证码
+export const sendSms = mobile => {
+  return request({
+    method: 'GET',
+    url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}

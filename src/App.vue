5e1980b57a2629e5933ae563c5880385d6bae289
1173079902 <1173079902@qq.com>
2021-02-24 10:19:31 +0800

    登录界面

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <h1>黑马头条</h1>
-    <i class="toutiao toutiaoshanchu"></i>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
     <router-view />
   </div>
 </template>
--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,8 @@
  */
 import axios from 'sxios'
 
+// 创建一个 axios 实例
+
 const request = axios.create({
   // 配置请求的基准URL地址
   baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径

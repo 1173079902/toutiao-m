3dbba93f6edb408c2348a6e0cc062926fe159b24
1173079902 <1173079902@qq.com>
2021-02-24 16:29:17 +0800

    存储TOKEN到VUEX和本地

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,17 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const TOKEN_KEY = 'TOUTIAO_USER'
+
 export default new Vuex.Store({
   state: {
+    user: JSON.parse(localStorage.getItem(TOKEN_KEY))
   },
   mutations: {
+    setUser(state, data) {
+      state.user = data
+      localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+    }
   },
   actions: {
   },

b5f48b6e88d5b3e80c35c611cc9ca507c6fd3412
1173079902 <1173079902@qq.com>
2021-02-24 19:12:16 +0800

    个人中心TabBar处理

---
+++ src/views/layout/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="layout-container">
+    <router-view />
+    <van-tabbar class="layout-tabbar" route>
+      <van-tabbar-item to="/">
+        <i slot="icon" class="toutiao toutiaoshouye"></i>
+        <span>首页</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <i slot="icon" class="toutiao toutiaowenda"></i>
+        <span>问答</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <i slot="icon" class="toutiao toutiaoshipin"></i>
+        <span>视频</span>
+      </van-tabbar-item>
+      <van-tabbar-item to="/my">
+        <i slot="icon" class="toutiao toutiaowode"></i>
+        <span>我的</span>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+.layout-container {
+  .layout-tabbar {
+    i.toutiao {
+      font-size: 40px;
+    }
+    span.text {
+      font-size: 20px;
+    }
+  }
+}
+</style>

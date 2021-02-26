5511a94e49279d0a2df536253a70f832f7a76eb5
1173079902 <1173079902@qq.com>
2021-02-26 15:31:26 +0800

    【首页】头部导航栏布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+        >搜索</van-button
+      >
+    </van-nav-bar>
+    <!-- /导航栏 -->
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage',
+  name: 'HomeIndex',
   components: {},
   props: {},
   data() {
@@ -18,4 +32,22 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.home-container {
+  /deep/.van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #f3f3f3;
+    border: none;
+    font-size: 28px;
+    color: black;
+    .van-icon {
+      font-size: 32px;
+      color: black;
+    }
+  }
+}
+</style>

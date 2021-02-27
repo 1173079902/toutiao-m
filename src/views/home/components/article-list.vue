c23cc87bca715ae3e063bbc85db13478b3ed360f
1173079902 <1173079902@qq.com>
2021-02-27 14:26:11 +0800

    【首页】【文章列表】处理响应结果

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -6,7 +6,11 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell
+        v-for="(article, index) in list"
+        :key="index"
+        :title="article.title"
+      />
     </van-list>
   </div>
 </template>
@@ -26,7 +30,8 @@ export default {
     return {
       list: [], // 存储列表数据的数组
       loading: false, // 控制加载中 loading 状态
-      finished: false // 控制数据加载结束的状态
+      finished: false, // 控制数据加载结束的状态
+      timestamp: null
     }
   },
   computed: {},
@@ -43,10 +48,17 @@ export default {
           timestamp: this.timestamp || Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳
           with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
         })
-        console.log(data)
         // 2. 把请求结果数据放到 list 数组中
+        const { results } = data.data
+        this.list.push(...results)
         // 3. 本次数据加载结束之后要把加载状态设置为结束
+        this.loading = false
         // 4. 判断数据是否全部加载完成
+        if (results.length) {
+          this.timestamp = data.data.pre_timestamp
+        } else {
+          this.finished = true
+        }
       } catch (err) {
         console.log(err)
       }

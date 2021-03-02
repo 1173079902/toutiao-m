3abb93804cb4ead3c90a043077a5c391f10b8498
1173079902 <1173079902@qq.com>
2021-03-02 14:44:58 +0800

    【文章搜索】点击取消返回

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="search-container">搜索页面</div>
+  <div class="search-container">
+    <!-- 搜索栏 -->
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        placeholder="请输入搜索关键词"
+        background="rgb(255 157 181)"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchPage'
+  name: 'SearchPage',
+  data() {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onSearch(val) {
+      console.log(val)
+    },
+    onCancel() {
+      this.$router.push(this.$route.query.redirect || '/')
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.search-container {
+  .van-search__action {
+    color: #fff;
+    &:active {
+      background-color: transparent;
+    }
+  }
+}
+</style>

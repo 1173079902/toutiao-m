72dd72ea2217bd90e54ca3842c0b99d25d6a7f49
1173079902 <1173079902@qq.com>
2021-03-02 16:23:07 +0800

    【文章搜索】【联想建议】请求数据并渲染

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="search-suggestion">
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
+    <van-cell
+      :title="text"
+      icon="search"
+      :key="index"
+      v-for="(text, index) in suggestions"
+    ></van-cell>
   </div>
 </template>
 
 <script>
+import { getSearchSuggestion } from '@/api/search.js'
 export default {
   name: 'SearchSuggestion',
   components: {},
@@ -19,20 +21,33 @@ export default {
     }
   },
   data() {
-    return {}
+    return {
+      suggestions: []
+    }
   },
   computed: {},
   watch: {
     searchText: {
+      // 监视的处理函数
       handler(value) {
-        console.log(value)
+        this.loadSearchSuggestion(value)
       },
+      // 首次监视触发
       immediate: true
     }
   },
   created() {},
   mounted() {},
-  methods: {}
+  methods: {
+    async loadSearchSuggestion(q) {
+      try {
+        const { data } = await getSearchSuggestion(q)
+        this.suggestions = data.data.options
+      } catch (err) {
+        this.$toast('获取失败')
+      }
+    }
+  }
 }
 </script>
 

71a3eb747b99cf9ee26deaddecd67814b525ef6f
1173079902 <1173079902@qq.com>
2021-03-03 16:26:30 +0800

    【文章详情】请求获取详情（出现了404）

--- src/api/article.js
+++ src/api/article.js
@@ -10,3 +10,13 @@ export const getArticles = params => {
     params
   })
 }
+
+/**
+ * 获取频道的文章列表
+ */
+export const getArticleById = articleId => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/articles/' + articleId
+  })
+}

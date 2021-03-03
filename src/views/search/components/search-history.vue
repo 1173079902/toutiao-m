2dcfbaad3c5e95215789c196a0c7a239d261f0b1
1173079902 <1173079902@qq.com>
2021-03-03 14:47:06 +0800

    【文章搜索】【历史记录】删除操作

--- src/views/search/components/search-history.vue
+++ src/views/search/components/search-history.vue
@@ -2,7 +2,7 @@
   <div class="search-history">
     <van-cell title="搜索历史">
       <div v-if="isDeleteShow">
-        <span>全部删除</span>
+        <span @click="$emit('clear-search-histories', [])">全部删除</span>
         &nbsp; &nbsp;
         <span @click="isDeleteShow = false">完成</span>
       </div>
@@ -12,6 +12,7 @@
       :title="item"
       v-for="(item, index) in searchHistories"
       :key="index"
+      @click="onSearchItemClick(item, index)"
     >
       <van-icon name="close" v-show="isDeleteShow" />
     </van-cell>
@@ -30,6 +31,15 @@ export default {
   data() {
     return {
       isDeleteShow: false // 控制删除显示状态
+    }
+  },
+  methods: {
+    onSearchItemClick(item, index) {
+      if (this.isDeleteShow) {
+        this.searchHistories.splice(index, 1)
+      } else {
+        this.$emit('search', item)
+      }
     }
   }
 }

c37ba747a8a54d951fa991b7affdcda98baad1e1
1173079902 <1173079902@qq.com>
2021-03-06 14:01:57 +0800

    【文章评论】【发布评论】成功后的处理

--- src/views/article/components/comment-post.vue
+++ src/views/article/components/comment-post.vue
@@ -36,13 +36,24 @@ export default {
   mounted() {},
   methods: {
     async onPost() {
+      this.$toast.loading({
+        message: '发布中...',
+        forbidClick: true, // 禁用背景点击
+        duration: 0 // 持续时间，0 代表持续展示，不关闭
+      })
       try {
         const { data } = await addComment({
           target: this.target, // 评论的目标 ID（评论文章即为文章ID，对评论进行回复则为评论ID）
           content: this.message, // 评论的内容
           art_id: null
         })
-        console.log(data)
+        // console.log(data)
+        // 关闭弹出层
+        // 将发布内容展示到列表顶部
+        // 清空文本框
+        this.message = ''
+        this.$emit('post-success', data.data)
+        this.$toast.success('发布成功')
       } catch (err) {
         this.$toast.fail('发布失败')
       }

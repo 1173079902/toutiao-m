31bd78e5b0334d8d5f77f505fdae47be06185db2
1173079902 <1173079902@qq.com>
2021-03-07 10:26:10 +0800

    【编辑资料】页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    用户资料
+  <div class="user-profile">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="page-nav-bar"
+      title="个人信息"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+    <!-- 个人信息 -->
+    <van-cell title="头像" is-link>
+      <van-image
+        class="avatar"
+        fit="cover"
+        round
+        src="https://img.yzcdn.cn/vant/cat.jpeg"
+      />
+    </van-cell>
+    <van-cell title="昵称" value="内容" is-link />
+    <van-cell title="性别" value="内容" is-link />
+    <van-cell title="生日" value="内容" is-link />
+    <!-- /个人信息 -->
   </div>
 </template>
 
@@ -16,4 +36,14 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.user-profile {
+  .avatar {
+    width: 60px;
+    height: 60px;
+  }
+  .van-popup {
+    background-color: #f5f7f9;
+  }
+}
+</style>

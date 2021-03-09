a9f97a1822aac013a1ee9ae180e6b063f87b9c73
1173079902 <1173079902@qq.com>
2021-03-09 11:10:52 +0800

    统一错误处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,7 @@
 import axios from 'axios'
 import store from '@/store'
 import jsonBig from 'json-bigint'
+import { Toast } from 'vant'
 
 // 创建一个 axios 实例
 
@@ -39,5 +40,29 @@ request.interceptors.request.use(function (config) {
   return Promise.reject(error)
 })
 
+// 响应拦截器
+request.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  const status = error.response.status
+  if (status === 400) {
+    Toast.fail('客户端请求参数错误')
+  } else if (status === 401) {
+    // 无效的 TOKEN
+    Toast.fail('无效的TOKEN')
+  } else if (status === 403) {
+    Toast.fail('客户端没有权限')
+  } else if (status === 404) {
+    Toast.fail('请求资源不存在')
+  } else if (status === 405) {
+    Toast.fail('请求方法不支持')
+  } else if (status >= 500) {
+    Toast.fail('服务器抽风了')
+  }
+  // 错误错误
+  return Promise.reject(error)
+})
 // 响应拦截器
 export default request
